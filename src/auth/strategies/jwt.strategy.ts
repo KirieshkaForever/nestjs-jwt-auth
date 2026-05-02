d53ca77d23b35bd624d142791a6256478db23f06
Kirieshka <kirieshka.forever@mail.ru>
2026-05-03 00:00:21 +0500

    Add comments

--- src/auth/strategies/jwt.strategy.ts
+++ src/auth/strategies/jwt.strategy.ts
@@ -20,6 +20,14 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
   }
 
   async validate(payload: JwtPayload) {
-    return await this.authService.validate(payload.id);
+    // Passport уже проверил подпись и срок действия токена.
+    // Здесь payload - это данные, которые были в токене
+
+    // Ищем актуального пользователя в БД по id (payload.id)
+    const user = await this.authService.validate(payload.id);
+
+    // То, что возвращает метод validate() стратегии,
+    // автоматически прикрепляется Passport'ом к объекту запроса как req.user
+    return user;
   }
 }
